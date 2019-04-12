8de39823f9bb680fdabf6130a59ce57352061cdb
Li <e1601126@edu.vamk.fi>
2019-04-12 15:26:12 +0300

    MyUser updated

--- src/main/java/fi/vamk/tka/reactspring/Initializer.java
+++ src/main/java/fi/vamk/tka/reactspring/Initializer.java
@@ -13,10 +13,9 @@ import java.util.HashSet;
 @Component
 class Initializer implements CommandLineRunner {
 
-    private  final GroupRepository repository;
-    private  final UserRepository userRepository;
-    private  final EventRepository eventRepository;
-
+    private final GroupRepository repository;
+    private final UserRepository userRepository;
+    private final EventRepository eventRepository;
 
     public Initializer(GroupRepository repository, UserRepository userRepository, EventRepository eventRepository) {
         this.repository = repository;
@@ -26,25 +25,21 @@ class Initializer implements CommandLineRunner {
 
     @Override
     public void run(String... strings) {
-        Stream.of("Denver JUG", "Utah JUG", "Seattle JUG",
-                "Richmond JUG").forEach(name ->
-                repository.save(new Group(name))
-        );
-        User a = new User();
+        Stream.of("Denver JUG", "Utah JUG", "Seattle JUG", "Richmond JUG")
+                .forEach(name -> repository.save(new Group(name)));
+        MyUser a = new MyUser();
         a.setName("Timo");
         userRepository.save(a);
-        User b = new User();
+        MyUser b = new MyUser();
         b.setName("Juhani");
         userRepository.save(b);
-        Set<User> users = new HashSet<User>();
+        Set<MyUser> users = new HashSet<MyUser>();
         users.add(a);
         users.add(b);
-    
+
         Group djug = repository.findByName("Denver JUG");
-        Event e = Event.builder().title("Full Stack Reactive")
-                .description("Reactive with Spring Boot + React")
-                .date(Instant.parse("2018-12-12T18:00:00.000Z"))
-                .build();
+        Event e = Event.builder().title("Full Stack Reactive").description("Reactive with Spring Boot + React")
+                .date(Instant.parse("2018-12-12T18:00:00.000Z")).build();
         e.setAttendees(users);
         djug.setEvents(Collections.singleton(e));
         repository.save(djug);
